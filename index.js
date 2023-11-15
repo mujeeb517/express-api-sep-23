6cf66d9eaac16b9053f30ea4b8ab68b0681c3658
Mujib <mujib@Mujibs-MacBook-Pro.local>
2023-11-15 08:36:00 +0530

    disable auth

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use('/', homeRouter);
 app.use('/api/users', userRouter);
 
 // app.use(basicAuth);
-app.use(tokenAuth);
+// app.use(tokenAuth);
 
 // private
 app.use('/api/products', productRoutes);
