8967718fa9494b5459b7b81591793db8e79db5e9
Mujib <mujib@Mujibs-MacBook-Pro.local>
2023-12-27 09:01:47 +0530

    allow cors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const cors = require('cors'); // commonjs
 const homeRouter = require('./routes/homeRouter');
 const productRoutes = require('./routes/productRouter');
 const userRouter = require('./routes/userRouter');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const path=require('path');
-const fs=require('fs');
-const config =require('./config');
+const path = require('path');
+const fs = require('fs');
+const config = require('./config');
 
 const { tokenAuth } = require('./middlewares/auth.js');
 
@@ -23,12 +24,15 @@ mongoose.connect(config.conStr)
     .then(() => console.log('Connected to Db'))
     .catch(err => console.log(err));
 
-const ws= fs.createWriteStream(path.join(__dirname,'logs', 'req.log'), { flags: 'a' });
+const ws = fs.createWriteStream(path.join(__dirname, 'logs', 'req.log'), { flags: 'a' });
+
+// allow all origins
+app.use(cors());
 
 app.use(morgan('dev'));
 
-app.use(morgan('common',{
-    stream:ws
+app.use(morgan('common', {
+    stream: ws
 }));
 
 app.use(bodyParser.json());
